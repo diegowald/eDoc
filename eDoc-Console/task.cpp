56eaa27e8dbf3d1f1d971d486fc1643fc2abf67b
Diego Wald <diego.wald@gmail.com>
2013-08-23 11:49:26 -0300

    Fixed (again) issue when trying to run console appl.

--- eDoc-Console/task.cpp
+++ eDoc-Console/task.cpp
@@ -1,13 +1,16 @@
 #include "task.h"
+#include <QsLog.h>
 
 Task::Task(const QString &appPath, QObject *parent) :
     QObject(parent)
 {
+    QLOG_TRACE() << "Task::Task(const QString &appPath, QObject *parent)";
     m_ApplicationPath = appPath;
 }
 
 void Task::run()
 {
+    QLOG_TRACE() << "void Task::run()";
     f.initialize(m_ApplicationPath, "./console.conf.xml", &logger);
 
     emit finished();
