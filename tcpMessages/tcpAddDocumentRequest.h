628ec90918011591be0595efda5d63ed56ed4441
Diego Ignacio Wald <diego.wald@gmail.com>
2013-09-23 21:09:31 -0300

    Added chunking functionality to AddDocumentRequest.

--- tcpMessages/tcpAddDocumentRequest.h
+++ tcpMessages/tcpAddDocumentRequest.h
@@ -4,21 +4,27 @@
 #include <QObject>
 #include "messageBase.h"
 #include "tcpmessages_global.h"
-
+#include "tcpchunck.h"
 
 class TCPMESSAGESSHARED_EXPORT AddDocumentRequest : public MessageBase
 {
     Q_OBJECT
 public:
-    explicit AddDocumentRequest(QObject *parent = 0);
+    explicit AddDocumentRequest(int chunkSize = 0, QObject *parent = 0);
     virtual ~AddDocumentRequest();
     virtual void setBlob(const QByteArray &blob);
+    virtual bool requiresSplit() const;
+    virtual TCPChunck *getChunk(int chunkId);
+    virtual int getChunkCount() const;
+
 signals:
     
 public slots:
 
 public:
     QByteArray m_Blob;
+private:
+    int m_ChunkSize;
 };
 
 TCPMESSAGESSHARED_EXPORT QDataStream& operator<<( QDataStream& dataStream, const AddDocumentRequest& message);
--- tcpMessages/tcpadddocumentrequest.cpp
+++ tcpMessages/tcpadddocumentrequest.cpp
@@ -1,8 +1,9 @@
 #include "tcpAddDocumentRequest.h"
 
-AddDocumentRequest::AddDocumentRequest(QObject *parent) :
+AddDocumentRequest::AddDocumentRequest(int chunkSize, QObject *parent) :
     MessageBase(ADD_DOCUMENT_REQ, parent)
 {
+    m_ChunkSize = chunkSize;
 }
 
 AddDocumentRequest::~AddDocumentRequest()
@@ -14,21 +15,45 @@ void AddDocumentRequest::setBlob(const QByteArray &blob)
     m_Blob = blob;
 }
 
+bool AddDocumentRequest::requiresSplit() const
+{
+    if (m_ChunkSize == 0)
+        return false;
+    return m_Blob.size() > m_ChunkSize;
+}
+
+int AddDocumentRequest::getChunkCount() const
+{
+    int count = m_Blob.size() / m_ChunkSize;
+    if (m_Blob.size() % m_ChunkSize > 0)
+        count++;
+    return count;
+}
+
+TCPChunck *AddDocumentRequest::getChunk(int chunkId)
+{
+    TCPChunck *ch = new TCPChunck(this);
+    int first = chunkId * m_ChunkSize;
+    ch->m_Blob = m_Blob.mid(first, m_ChunkSize);
+    return ch;
+}
+
 QDataStream& operator<<(QDataStream& dataStream, const AddDocumentRequest& message)
 {
     dataStream << (*(MessageBase*)(&message))
-    /*dataStream << message.messageType
-               << message.messageSize*/
-               << message.m_Blob;
+               << message.getChunkCount();
+    if (!message.requiresSplit())
+        dataStream << message.m_Blob;
     return dataStream;
 }
 
 // Important: this will throw a UserException on error
 QDataStream& operator>>(QDataStream& dataStream, AddDocumentRequest& message) // deprecated: throw( UserException )
 {
-    int aux = 0;
-    dataStream >> (*(MessageBase*)(&message))
-               >> message.m_Blob;
-    message.messageType = (MessageType) aux;
+    dataStream >> (*(MessageBase*)(&message));
+    int chunkCount = 0;
+    dataStream >> chunkCount;
+    if (chunkCount < 2)
+        dataStream >> message.m_Blob;
     return dataStream;
 }
